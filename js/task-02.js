7450787a396e8efabb648808d4f90b4f9b257917
Cristina-Tataru <151068609+Cristina-Tataru@users.noreply.github.com>
2024-04-15 16:10:13 +0300

    commit js 6

---
+++ js/task-02.js
@@ -0,0 +1,28 @@
+
+  const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+  
+  const ingredientList = document.querySelector('#ingredients');
+  
+  const elements = ingredients.map(ingredient => {
+    const ingredientElement = document.createElement('li');
+    ingredientElement.textContent = ingredient;
+    return ingredientElement;
+  });
+  
+  ingredientList.append(...elements);
+  
+  // ingredients.forEach(ingredient => {
+  //   const ingredientElement = document.createElement('li');
+  //   ingredientElement.textContent = ingredient;
+  
+  //   ingredientList.appendChild(ingredientElement);
+  
+  //   console.log(ingredientElement);
+  // });
